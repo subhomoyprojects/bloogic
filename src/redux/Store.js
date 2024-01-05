2b6cb25fe9347a1e556b185495a7551563419a56
webskitters <webskitters@webskitterss-iMac.local>
2024-01-05 19:56:35 +0530

    add testimonials slice

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./slice/AuthSlice";
 import BlogSlice from "./slice/BlogSlice";
+import TestimonialsSlice from "./slice/TestimonialsSlice";
 
 const Store = configureStore({
   reducer: {
     Auth: AuthSlice,
     Blog: BlogSlice,
+    Testimonials: TestimonialsSlice,
   },
 });
 
---
+++ src/redux/slice/TestimonialsSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosInstance, { status } from "../Helper";
+
+export const TestimonialsItems = createAsyncThunk("/testimonial", async () => {
+  let res = await axiosInstance.get("/testimonial");
+  let resData = res?.data;
+  return resData;
+});
+
+const TestimonialsSlice = createSlice({
+  name: "TestimonialsSlice",
+  initialState: {
+    status: status.idle,
+    testimonialsItems: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(TestimonialsItems.pending, (state) => {
+        state.status = status.loading;
+      })
+      .addCase(TestimonialsItems.fulfilled, (state, { payload }) => {
+        state.status = status.idle;
+        state.testimonialsItems = payload.data;
+      })
+      .addCase(TestimonialsItems.rejected, (state) => {
+        state.status = status.error;
+      });
+  },
+});
+
+export default TestimonialsSlice.reducer;
